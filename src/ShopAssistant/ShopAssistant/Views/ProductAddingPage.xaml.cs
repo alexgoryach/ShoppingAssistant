e06d71b543dd3f163cc67c719a96befa6f24066d
Alexandr Goryachkin <alexandr.goryach@gmail.com>
2022-05-02 19:22:07 +0700

    Initial commit

---
+++ src/ShopAssistant/ShopAssistant/Views/ProductAddingPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopAssistant.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductAddingPage : ContentPage
+    {
+        public ProductAddingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/ShopAssistant/ShopAssistant/Views/ShoppingListPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShopAssistant.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShoppingListPage : ContentPage
+    {
+        public ShoppingListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
