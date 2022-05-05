ed6f25a557feaa11d78074bb768394a85f64b78c
Alexandr Goryachkin <alexandr.goryach@gmail.com>
2022-05-06 00:06:57 +0700

    ShoppingListPage changes

--- src/ShopAssistant/ShopAssistant/Views/ShoppingListPage.xaml.cs
+++ src/ShopAssistant/ShopAssistant/Views/ShoppingListPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using ShopAssistant.Domain;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace ShopAssistant.Views
@@ -9,6 +11,28 @@ namespace ShopAssistant.Views
         public ShoppingListPage()
         {
             InitializeComponent();
+            //BindingContext = new ProductListViewModel() { Navigation = this.Navigation };
+        }
+        protected override void OnAppearing()
+        {
+            productsList.ItemsSource = App.Database.GetItems();
+            base.OnAppearing();
+        }
+
+        private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Product selectedProduct = (Product) e.SelectedItem;
+            ProductAddingPage productAddingPage = new ProductAddingPage();
+            productAddingPage.BindingContext = selectedProduct;
+            await Navigation.PushAsync(productAddingPage);
+        }
+
+        private async void CreateProduct(object sender, EventArgs e)
+        {
+            Product friend = new Product();
+            ProductAddingPage productAddingPage = new ProductAddingPage();
+            productAddingPage.BindingContext = friend;
+            await Navigation.PushAsync(productAddingPage);
         }
     }
 }
